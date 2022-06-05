e59ecbbb507b4d2df8ed00cf24eff2c89061e1e8
David Ongaro <davidongaroh@gmil.com>
2022-06-05 23:57:16 +0300

    event listener added

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
